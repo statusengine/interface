659786125981acee76a59caf6868570ac77beae0
Daniel Ziegler <info@nook24.eu>
2017-09-14 21:17:13 +0200

    Fix auto refresh - whoops and ad ajax loader to menu

--- public/js/scripts/controllers/IndexController.js
+++ public/js/scripts/controllers/IndexController.js
@@ -1,12 +1,13 @@
 angular.module('Statusengine')
-    .controller('IndexController', function ($scope, ReloadService, $http, $interval) {
+    .controller('IndexController', function ($scope, ReloadService, $http, $interval, $timeout) {
         $scope.reload = ReloadService.triggerReload;
 
         $scope.date = {};
         $scope.acknowledgements = 0;
         $scope.downtimes = 0;
 
-        $scope.reload = function () {
+        $scope.loadMenustats = function () {
+            $('#menustatsRefresh').show();
             $http.get("/api/index.php/menustats").then(function (result) {
                     $scope.data = result.data;
                     $scope.acknowledgements = $scope.data.number_of_host_acknowledgements;
@@ -14,11 +15,15 @@ angular.module('Statusengine')
 
                     $scope.downtimes = $scope.data.number_of_scheduled_host_downtimes;
                     $scope.downtimes += $scope.data.number_of_scheduled_service_downtimes;
+                    $timeout(function () {
+                        //Let the weel spin for one seconds
+                        $('#menustatsRefresh').hide();
+                    }, 1000);
                 }
             );
         };
 
 
-        $interval($scope.reload, 30000);
-        $scope.reload();
+        $interval($scope.loadMenustats, 30000);
+        $scope.loadMenustats();
     });

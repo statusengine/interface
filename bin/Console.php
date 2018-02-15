653066727d294f1ec336a49aa8205ad8cc6b8b03
Daniel Ziegler <info@nook24.eu>
2018-02-15 19:33:42 +0100

    Update Copyright

--- bin/Console.php
+++ bin/Console.php
@@ -3,7 +3,7 @@
 
 /**
  * Statusengine UI
- * Copyright (C) 2016-2017  Daniel Ziegler
+ * Copyright (C) 2016-2018  Daniel Ziegler
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
--- src/Loader/LogentryLoaderInterface.php
+++ src/Loader/LogentryLoaderInterface.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Statusengine UI
- * Copyright (C) 2016-2017  Daniel Ziegler
+ * Copyright (C) 2016-2018  Daniel Ziegler
 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
